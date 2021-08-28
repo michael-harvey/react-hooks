76299bdb1a948a551dd788cac5e06f8ac8a0e795
Mike Harvey <harvey.miche@gmail.com>
2021-08-29 11:07:51 +1200

    Adds comments to useLocalStorageState hook

--- useLocalStorageState.js
+++ useLocalStorageState.js
@@ -1,9 +1,23 @@
 import { useState, useEffect, useRef } from "react";
 
-function useLocalStorageState(key, initialData) {
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(key)) || initialData
-  );
+/**
+ * localStorage hook for setting and updating localStorage state
+ * 
+ * @param {string} localStorage key 
+ * @param {any} initialState value
+ * @returns [state, updater]
+ */
+function useLocalStorageState(key, initialState) {
+  const [state, setState] = useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key);
+    
+    if (valueInLocalStorage) {
+      return JSON.parse(valueInLocalStorage)
+    }
+    
+    // if a function is passed, call it to get the return value
+    return typeof initialState === 'function' ? initialState() : initialState;
+  });
 
   // keep state of key so if it changes we can reference this value for comparison
   const previousKeyRef = useRef(key);
@@ -16,10 +30,10 @@ function useLocalStorageState(key, initialData) {
       window.localStorage.removeItem(previousKey);
     }
 
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
 
-  return [value, setValue];
+  return [state, setState];
 }
 
 export default useLocalStorageState;
